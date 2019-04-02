3c6804df72eba3c2d620e5d63b9ebabe1f531fea
simonenkoav <simonenko1807@gmail.com>
2019-04-02 17:02:39 +0300

    Add imdb lists scraper

---
+++ country_films_crawler/country_films_crawler/spiders/imdb_lists_spider.py
@@ -0,0 +1,72 @@
+import scrapy
+from html.parser import HTMLParser
+import functools
+
+
+class PhotoPageHTMLParser(HTMLParser):
+    content = ''
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'meta':
+            attrs_d = dict(attrs)
+            if 'itemprop' in attrs_d and attrs_d['itemprop'] == 'image':
+                self.content = attrs_d['content']
+
+
+class ImdbListsSpider(scrapy.Spider):
+    name = "imdb_list"
+
+    gained_data = []
+
+    gained_actors = []
+
+    urls = ['https://www.imdb.com/list/ls077130929/']
+
+    photo_page_html_parser = PhotoPageHTMLParser()
+
+    def save_data(self):
+        self.gained_data = list(set(self.gained_data))
+        self.log('GAINED %d' % len(self.gained_data))
+        with open('../data/black_lists_imdb_data.txt', 'w') as fd:
+            for url in self.gained_data:
+                fd.write(url + '\n')
+
+    def start_requests(self):
+        for url in self.urls:
+            yield scrapy.Request(url=url, callback=self.parse_list)
+
+    def parse_list(self, response):
+        for info in response.css('div.lister-list div.lister-item h3.lister-item-header'):
+            name = info.css('a::text').get().replace(' ', '')
+            if name in self.gained_actors:
+                return
+            else:
+                self.gained_actors.append(name)
+            actor_url = info.css('a::attr(href)')[0]
+            yield response.follow(url=actor_url, callback=self.parse_actor)
+
+        for href in response.css('div.list-pagination a.next-page::attr(href)'):
+            yield response.follow(url=href, callback=self.parse_list)
+
+    def parse_actor(self, response):
+        photos_url = response.css('div.see-more a::attr(href)')[0]
+        yield response.follow(url=photos_url, callback=self.parse_photos)
+
+    def parse_photos(self, response):
+        for href in response.css('div.media_index_thumb_list a::attr(href)'):
+            yield response.follow(url=href, callback=self.parse_single_photo)
+
+    @staticmethod
+    def formate_photo_url(url):
+        return url[:url.find('_V1_')+4] + '.jpg'
+
+    def parse_single_photo(self, response):
+        self.photo_page_html_parser.feed(str(response.body))
+        photo_url = self.photo_page_html_parser.content
+        photo_url = self.formate_photo_url(photo_url)
+        self.gained_data.append(photo_url)
+        self.save_data()
+
+    def close(self, spider, reason):
+        self.save_data()
+        super(ImdbListsSpider, self).close(spider, reason)
--- country_films_crawler/country_films_crawler/spiders/kinopoisk_spider.py
+++ country_films_crawler/country_films_crawler/spiders/kinopoisk_spider.py
@@ -10,12 +10,28 @@ class ContryFilmsSpider(scrapy.Spider):
                        'Корея': 156, 'Корея Северная': 137, 'Корея Южная': 26, 'Кыргызстан': 86, 'Малайзия': 83,
                        'Монголия': 132, 'Мьянма': 1034, 'Сиам': 1021, 'Сингапур': 45, 'Таджикистан': 70, 'Таиланд': 44,
                        'Тайвань': 27, 'Туркменистан': 152, 'Узбекистан': 71, 'Филиппины': 47, 'Япония': 9}
+                        # + Бирма: 148, Бутан: 114, Лаос: 149
+
+    arabic_countries = {'Алжир': 20, 'Азербайджан': 136, 'Албания': 120, 'Армения': 89, 'Афганистан': 113,
+                        'Бахрейн': 164, 'Египет': 101, 'Западная Сахара': 1043, 'Израиль': 42, 'Индия': 29,
+                        'Иордания': 154, 'Ирак': 90, 'Иран': 48, 'Йемен': 169, 'Катар': 1002, 'Кувейт': 147,
+                        'Ливан': 97, 'Ливия': 126, 'Мавритания': 67, 'Марокко': 43, 'ОАЭ': 119, 'Оман': 1003,
+                        'Пакистан': 74, 'Палестина': 78, 'Саудовская Аравия': 158, 'Сирия': 98, 'Судан': 167,
+                        'Тунис': 50}
 
     main_url = 'https://www.kinopoisk.ru/lists/m_act[country]/%d'
 
+    def save_data(self):
+        self.gained_data = list(set(self.gained_data))
+        self.log('GAINED %d' % len(self.gained_data))
+        with open('../data/arabic_gained_data.txt', 'w') as fd:
+            for pname, url in self.gained_data:
+                line = pname.replace(' ', '_') + ' ' + url
+                fd.write(line + '\n')
+
     def start_requests(self):
-        urls = [self.main_url % code for code in self.asian_countries.values()]
-        
+        urls = [self.main_url % code for code in self.arabic_countries.values()]
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse_country)
 
@@ -23,7 +39,7 @@ class ContryFilmsSpider(scrapy.Spider):
         for film_info in response.css('div.tenItems div.item'):
             film_url = film_info.css('div.info div.name a::attr(href)')[0]
             film_countries = film_info.css('div.flags div.flag a::attr(title)').getall()
-            if all(c in self.asian_countries for c in film_countries):
+            if all(c in self.arabic_countries for c in film_countries):
                 yield response.follow(url=film_url, callback=self.parse_film)
 
         for href in response.css('div.navigator li.arr a::attr(href)'):
@@ -31,7 +47,6 @@ class ContryFilmsSpider(scrapy.Spider):
 
     def parse_film(self, response):
         cast_url = response.css('#actorList h4 a::attr(href)')[0]
-        self.log('cast_url ' + str(cast_url.get()))
         yield response.follow(url=cast_url, callback=self.parse_cast)
 
     def parse_cast(self, response):
@@ -53,12 +68,8 @@ class ContryFilmsSpider(scrapy.Spider):
         img_url = response.css('#main_table td img::attr(src)').get()
         person_name = response.css('#main_table td img::attr(alt)').get()[:-1]  # last symbol of name is space
         self.gained_data.append((person_name, img_url))
+        self.save_data()
 
     def close(self, spider, reason):
-        self.gained_data = list(set(self.gained_data))
-        self.log('GAINED %d' % len(self.gained_data))
-        with open('../data/asians_gained_data.txt', 'w') as fd:
-            for pname, url in self.gained_data:
-                line = pname.replace(' ', '_') + ' ' + url
-                fd.write(line + '\n')
+        self.save_data()
         super(ContryFilmsSpider, self).close(spider, reason)
